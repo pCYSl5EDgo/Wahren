23319845bde0e62644bd77180c19c02c970d12ab
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2018-06-09 14:32:05 +0900

    [update]Add Target Framework
    .net core 2.0をターゲットフレームワークに追加
    対応してdotnetcore2.1特有の部分を#ifで囲んだ

---
+++ DeconstructHelper.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+namespace Wahren
+{
+    public static class DeconstructHelper{
+        public static void Deconstruct<T1, T2>(this KeyValuePair<T1, T2> tuple, out T1 key, out T2 value)
+        {
+            key = tuple.Key;
+            value = tuple.Value;
+        }
+    }
+}
