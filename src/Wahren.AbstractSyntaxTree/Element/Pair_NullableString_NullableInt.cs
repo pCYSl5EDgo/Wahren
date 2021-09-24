2f5e4e0980fb7819439f902360c31102c9206a5f
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2021-09-24 23:14:59 +0900

    More Specific

--- src/Wahren.AbstractSyntaxTree/Element/Pair_NullableString_NullableInt.cs
+++ src/Wahren.AbstractSyntaxTree/Element/Pair_NullableString_NullableInt.cs
@@ -4,6 +4,9 @@ public struct Pair_NullableString_NullableInt
 {
     public uint Text;
     public int Number;
+    public uint ReferenceId;
     public bool HasText;
     public bool HasNumber;
+    public bool HasReference;
+    public NodeKind ReferenceKind;
 }
