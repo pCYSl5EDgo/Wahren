ee44d400c7951bed76f0e955eb98a4c21a2cefe7
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2021-10-02 13:16:28 +0900

    32byte

--- src/Wahren.AbstractSyntaxTree/Statement/Argument.cs
+++ src/Wahren.AbstractSyntaxTree/Statement/Argument.cs
@@ -1,11 +1,34 @@
-﻿namespace Wahren.AbstractSyntaxTree.Statement;
+﻿using System.Runtime.InteropServices;
 
+namespace Wahren.AbstractSyntaxTree.Statement;
+
+[StructLayout(LayoutKind.Explicit, Pack = 32, Size = 32)]
 public struct Argument
 {
+    [FieldOffset(0)]
     public uint TokenId;
+    [FieldOffset(4)]
+    public uint TrailingTokenCount;
+    [FieldOffset(8)]
     public int Number;
-    public ReferenceKind ReferenceKind;
+    [FieldOffset(12)]
     public uint ReferenceId;
+    [FieldOffset(16)]
+    public ReferenceKind ReferenceKind;
+    [FieldOffset(24)]
     public bool IsNumber;
+    [FieldOffset(25)]
     public bool HasReference;
+    [FieldOffset(26)]
+    private bool _0;
+    [FieldOffset(27)]
+    private bool _1;
+    [FieldOffset(28)]
+    private bool _2;
+    [FieldOffset(29)]
+    private bool _3;
+    [FieldOffset(30)]
+    private bool _4;
+    [FieldOffset(31)]
+    private bool _5;
 }
