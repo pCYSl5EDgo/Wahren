c5973673153c5e29ce8379303d10fa430bcd9903
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2021-10-21 17:46:42 +0900

    Element modify

--- src/Wahren.AbstractSyntaxTree.Modifier/PerFileModifier.cs
+++ src/Wahren.AbstractSyntaxTree.Modifier/PerFileModifier.cs
@@ -1,4 +1,6 @@
-﻿global using Wahren.AbstractSyntaxTree.Parser;
+﻿global using Wahren.AbstractSyntaxTree;
+global using Wahren.AbstractSyntaxTree.Node;
+global using Wahren.AbstractSyntaxTree.Parser;
 global using Wahren.AbstractSyntaxTree.Element;
 global using Wahren.AbstractSyntaxTree.Statement;
 global using Wahren.AbstractSyntaxTree.Statement.Expression;
@@ -7,9 +9,29 @@ global using System;
 
 namespace Wahren.AbstractSyntaxTree.Modifier;
 
-public static class PerFileModifier
+public static partial class PerFileModifier
 {
-    public static void InsertLine(ref this Result result, int lineIndex)
+    public static void RemoveRange(ref Result result, uint tokenIndex, uint removeCount)
+    {
+        if (removeCount == 0)
+        {
+            return;
+        }
+
+        result.DecrementToken(tokenIndex, removeCount);
+        ref var tokenList = ref result.TokenList;
+        var firstLine = tokenList.GetLine(tokenIndex);
+        var endIndex = tokenIndex + removeCount;
+        var endLine = tokenList.GetLine(endIndex);
+        var firstOffset = tokenList.GetOffset(tokenIndex);
+        var firstPrecedingWhitespaceCount = tokenList.GetPrecedingWhitespaceCount(tokenIndex);
+        var endOffset = tokenList.GetOffset(endIndex);
+        var endLength = tokenList.GetLength(endIndex);
+        result.Source.RemoveRange(firstLine, firstOffset - firstPrecedingWhitespaceCount, endLine, endOffset + endLength);
+        result.TokenList.RemoveRange((int)tokenIndex, (int)removeCount);
+    }
+
+    public static void InsertLine(ref Result result, uint lineIndex)
     {
         result.Source.InsertEmpty(lineIndex);
         ref var tokenList = ref result.TokenList;
@@ -23,12 +45,7 @@ public static class PerFileModifier
             }
         }
 
-        if (lineSpan[i]++ == lineIndex && tokenList.GetOffset((uint)i) == tokenList.GetPrecedingWhitespaceCount((uint)i))
-        {
-            tokenList.GetPrecedingNewLineCount((uint)i)++;
-        }
-
-        for (++i; i < lineSpan.Length; ++i)
+        for (; i < lineSpan.Length; ++i)
         {
             lineSpan[i]++;
         }
@@ -36,7 +53,7 @@ public static class PerFileModifier
 
     public static void IncreaseOffset(ref TokenList tokenList, uint lineIndex, uint startTokenIndex, uint diff)
     {
-        for (; startTokenIndex < tokenList.Count ; ++startTokenIndex)
+        for (; startTokenIndex < tokenList.Count; ++startTokenIndex)
         {
             if (tokenList.GetKind(startTokenIndex) == TokenKind.Deleted)
             {
@@ -118,5 +135,77 @@ public static class PerFileModifier
 
         AdjustCopy(ref result, tokenIndex, singleLineSpan);
         return true;
+    }
+
+    public static void CreateEmptyElement<TNode, TElement>(ref Result result, ref TNode node, out TElement element, ReadOnlySpan<char> elementSpan, ReadOnlySpan<char> variationSpan, TokenKind elementTokenKind)
+        where TNode : struct, INode
+        where TElement : class, IElement
+    {
+        var insertRangeStartIndex = node.BracketRight - 1U;
+        result.TokenList.InsertUndefinedRange((int)insertRangeStartIndex, 3, out var undefinedRange);
+        result.IncrementToken(insertRangeStartIndex, 3U);
+        var bracketRightLine = result.TokenList.GetLine(node.BracketRight);
+        ref var newLineCount = ref result.TokenList.GetPrecedingNewLineCount(node.BracketRight);
+        var oldNewLineCount = newLineCount;
+        newLineCount = 1U;
+        InsertLine(ref result, bracketRightLine);
+        ref var line = ref result.Source[bracketRightLine];
+        var additionalVariationSize = variationSpan.IsEmpty ? 0U : 1U + (uint)variationSpan.Length;
+        var preparedSpan = line.InsertUndefinedRange(0, 8U + (uint)elementSpan.Length + additionalVariationSize);
+        "    ".CopyTo(preparedSpan);
+        elementSpan.CopyTo(preparedSpan.Slice(4));
+        preparedSpan = preparedSpan.Slice(elementSpan.Length + 4);
+        if (!variationSpan.IsEmpty)
+        {
+            preparedSpan[0] = '@';
+            variationSpan.CopyTo(preparedSpan.Slice(1));
+            preparedSpan = preparedSpan.Slice(variationSpan.Length + 1);
+        }
+
+        " = @".CopyTo(preparedSpan);
+        undefinedRange.kindSpan[0] = elementTokenKind;
+        undefinedRange.kindSpan[1] = TokenKind.Assign;
+        undefinedRange.kindSpan[2] = TokenKind.Content;
+        undefinedRange.lengthSpan[0] = (uint)elementSpan.Length + additionalVariationSize;
+        undefinedRange.lengthSpan[1] = 1U;
+        undefinedRange.lengthSpan[2] = 1U;
+        undefinedRange.lineSpan.Fill(bracketRightLine);
+        undefinedRange.offsetSpan[0] = 4U;
+        undefinedRange.offsetSpan[1] = 5U + (uint)elementSpan.Length + additionalVariationSize;
+        undefinedRange.offsetSpan[2] = 7U + (uint)elementSpan.Length + additionalVariationSize;
+        undefinedRange.otherSpan.Clear();
+        undefinedRange.precedingNewLineCountSpan[0] = oldNewLineCount;
+        undefinedRange.precedingNewLineCountSpan[1] = 0U;
+        undefinedRange.precedingNewLineCountSpan[2] = 0U;
+        undefinedRange.precedingWhitespaceCountSpan[0] = 4U;
+        undefinedRange.precedingWhitespaceCountSpan[1] = 1U;
+        undefinedRange.precedingWhitespaceCountSpan[2] = 1U;
+        element = (TElement)TElement.Create(insertRangeStartIndex, elementSpan.Length, !variationSpan.IsEmpty);
+    }
+
+    public static void ForceElementEmpty(ref Result result, Pair_NullableString_NullableIntElement element)
+    {
+        if (!element.HasValue)
+        {
+            return;
+        }
+
+        ref var value = ref element.Value;
+        var valueText = value.Text;
+        uint diff;
+        if (value.HasNumberTokenId)
+        {
+            diff = value.NumberTokenId - valueText;
+        }
+        else
+        {
+            diff = value.TrailingTokenCount;
+        }
+
+        RemoveRange(ref result, valueText + 1U, diff);
+        AdjustCopy(ref result, valueText, "@");
+        value = default;
+        value.Text = valueText;
+        element.HasValue = false;
     }
 }
