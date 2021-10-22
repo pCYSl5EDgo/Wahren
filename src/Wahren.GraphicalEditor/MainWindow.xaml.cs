70edb91eab3ea119749eb1a0f34863b36f5e8c5e
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2021-10-23 04:16:56 +0900

    Metro

--- src/Wahren.GraphicalEditor/MainWindow.xaml.cs
+++ src/Wahren.GraphicalEditor/MainWindow.xaml.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using MahApps.Metro.Controls;
 
-namespace Wahren.GraphicalEditor
+namespace Wahren.GraphicalEditor;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : MetroWindow
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
