cbd0ef8971362ab3779766a4489acfb1ee158113
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2021-10-28 01:50:44 +0900

    Utility

---
+++ src/Wahren.GraphicalEditor/ReadFileUtility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Wahren.GraphicalEditor;
+
+internal static class ReadFileUtility
+{
+    public static async ValueTask<(byte[] array, int size)> ReadAsync(string path, CancellationToken token)
+    {
+        using var handle = File.OpenHandle(path, FileMode.Open, FileAccess.Read, FileShare.Read, FileOptions.Asynchronous);
+        var length = RandomAccess.GetLength(handle);
+        if (length == 0)
+        {
+            return (Array.Empty<byte>(), 0);
+        }
+
+        if (length > int.MaxValue)
+        {
+            throw new InsufficientMemoryException(path);
+        }
+
+        var buffer = ArrayPool<byte>.Shared.Rent((int)length);
+        try
+        {
+            var actual = await RandomAccess.ReadAsync(handle, buffer.AsMemory(0, (int)length), 0, token).ConfigureAwait(false);
+            return (buffer, actual);
+        }
+        catch
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+            throw;
+        }
+    }
+}
