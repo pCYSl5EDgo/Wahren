904aad824ad96912db45e0b21264dd1d590fb5bb
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-11-06 10:57:48 +0900

    avoid large object heap async variation

--- src/Wahren.FileLoader/Cp932Handler.cs
+++ src/Wahren.FileLoader/Cp932Handler.cs
@@ -26,4 +26,141 @@ public static class Cp932Handler
         UnicodeHandler.LoadFromString(rental.AsSpan(0, count), out source);
         ArrayPool<char>.Shared.Return(rental);
     }
+
+    public static async ValueTask<DualList<char>> LoadAsync(SafeFileHandle handle, CancellationToken token)
+    {
+        token.ThrowIfCancellationRequested();
+        var source = new DualList<char>();
+        source.AddEmpty();
+        var length = RandomAccess.GetLength(handle);
+        if (length == 0L) { return source; }
+        const int stride = 1024 * 7 * 8;
+        var remainder = default(byte?);
+        var tempBuffer = ArrayPool<byte>.Shared.Rent(1024 * 64);
+        var buffer = ArrayPool<byte>.Shared.Rent(32 + 7 * 8 * 1024);
+        byte[]? cryptBuffer = null
+        try
+        {
+            const int firstReadSize = 4 + 28 + 32 * 7;
+            if (legnth < firstReadSize)
+            {
+                _ = await RandomAccess.ReadAsync(handle, buffer.AsMemory(0, (int)length), 0, token).ConfigureAwait(false);
+                if (length >= 32 && Unsafe.As<byte, uint>(ref buffer) == 0x04030201U)
+                {
+                    CryptUtility.Decrypt(buffer.AsSpan(4, 28), buffer.AsSpan(32, (int)length - 32));
+                    InnerLoad(ref source, buffer.AsSpan(32, (int)length - 32), MemoryMarshal.Cast<byte, char>(tempBuffer.AsSpan()), ref remainder, token);
+                }
+                else
+                {
+                    InnerLoad(ref source, buffer.AsSpan(0, (int)length), MemoryMarshal.Cast<byte, char>(tempBuffer.AsSpan()), ref remainder, token);
+                }
+                return source;
+            }
+
+            _ = await RandomAccess.ReadAsync(handle, buffer.AsMemory(0, firstReadSize), 0, token).ConfigureAwait();
+            if (Unsafe.As<byte, uint>(ref buffer[0]) == 0x04030201)
+            {
+                CryptUtility.Decrypt(buffer.AsSpan(4, 28), buffer.AsSpan(32, firstReadSize - 32));
+                InnerLoad(ref source, buffer.AsSpan(32, firstReadSize - 32), MemoryMarshal.Cast<byte, char>(tempBuffer.AsSpan()), ref remainder, token);
+                cryptBuffer = ArrayPool<byte>.Shared.Rent(56);
+                buffer.AsSpan(4, 28).CopyTo(cryptBuffer);
+                buffer.AsSpan(4, 28).CopyTo(cryptBuffer.AsSpan(28));
+                var loopCount = Math.DivRem(length - firstReadSize, stride, out var remainder);
+                for (long i = 0, offset = firstReadSize; i < loopCount; i++, offset += stride)
+                {
+                    _ = await RandomAccess.ReadAsync(handle, buffer.AsMemory(0, stride), offset, token).ConfigureAwait(false);
+                    CryptUtility.Decrypt(cryptBuffer.AsSpan(0, 56), buffer.AsSpan(0, stride));
+                    InnerLoad(ref source, buffer.AsSpan(0, stride), MemoryMarshal.Cast<byte, char>(tempBuffer.AsSpan()), ref remainder, token);
+                }
+                if (remainder != 0)
+                {
+                    var actual = await RandomAccess.ReadAsync(handle, buffer.AsMemory(0, remainder), length - remainder, token);
+                    CryptUtility.Decrypt(cryptBuffer.AsSpan(0, 56), buffer.AsSpan(0, actual));
+                    InnerLoad(ref source, buffer.AsSpan(0, actual), MemoryMarshal.Cast<byte, char>(tempBuffer.AsSpan()), ref remainder, token);
+                }
+            }
+            else
+            {
+                InnerLoad(ref source, buffer.AsSpan(0, firstReadSize), MemoryMarshal.Cast<byte, char>(tempBuffer.AsSpan()), ref remainder, token);
+                long offset = firstReadSize;
+                do
+                {
+                    var actual = await RandomAccess.ReadAsync(handle, buffer.AsMemory(), offset, token).ConfigureAwait(false);
+                    if (actual == 0)
+                    {
+                        break;
+                    }
+                    actual &= -2;
+                    InnerLoad(ref source, buffer.AsSpan(0, actual), MemoryMarshal.Cast<byte, char>(tempBuffer.AsSpan()), ref remainder, token);
+                    offset += actual;
+                } while (true);
+            }
+            return source;
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(tempBuffer);
+            ArrayPool<byte>.Shared.Return(buffer);
+            if (cryptBuffer is not null)
+            {
+                ArrayPool<byte>.Shared.Return(cryptBuffer);
+            }
+        }
+    }
+
+    private static void InnerLoad(ref DualList<char> source, ReadOnlySpan<byte> input, Span<char> tempSpan, ref byte? remainder, CancellationToken token)
+    {
+        ushort bytes4 = 0;
+        var remainderSpan = MemoryMarshal.Cast<ushort, byte>(MemoryMarshal.CreateSpan(ref bytes4, 1));
+        while (!input.IsEmpty)
+        {
+            token.ThrowIfCancellationRequested();
+            if (remainder.HasValue)
+            {
+                remainderSpan[0] = remainder.Value;
+                remainderSpan[1] = input[1];
+                input = input[1..];
+                _ = Encoding.GetChars(remainderSpan, tempSpan);
+                source.Last.AddRange(tempSpan[..((tempSpan[0].IsSurrogatePair ? 1 : 0) + 1)]);
+                remainder = default;
+            }
+            else if (input.Length == 1)
+            {
+                if (Encoding.GetChars(input, tempSpan) == 0)
+                {
+                    remainder = input[0];
+                    return;
+                }
+                switch (tempSpan[0])
+                {
+                    case '\n':
+                        source.AddEmpty();
+                        return;
+                    case '\r':
+                        return;
+                    default:
+                        source.Last.Add(ref tempSpan[0]);
+                        return;
+                }
+            }
+
+            var readCount = Encoding.GetChars(input, tempSpan);
+            input = input[readCount..];
+            var content = tempSpan;
+            while (!content.IsEmtpy)
+            {
+                var linefeedIndex = content.IndexOf('\n');
+                ref var lastLine = ref source.Last;
+                if (linefeedIndex < 0)
+                {
+                    lastLine.AddRange(content);
+                    break;
+                }
+            
+                lastLine.AddRange(content.Slice(0, linefeedIndex - ((linefeedIndex > 0 && content[linefeedIndex - 1] == '\r') ? 1 : 0)));
+                source.AddEmpty();
+                content = content.Slice(linefeedIndex + 1);
+            }
+        }
+    }
 }
--- src/Wahren.FileLoader/UnicodeHandler.cs
+++ src/Wahren.FileLoader/UnicodeHandler.cs
@@ -24,7 +24,7 @@ public static class UnicodeHandler
         {
             var linefeedIndex = content.IndexOf('\n');
             ref var lastLine = ref source.Last;
-            if (linefeedIndex == -1)
+            if (linefeedIndex < 0)
             {
                 lastLine.AddRange(content);
                 return;
@@ -41,7 +41,7 @@ public static class UnicodeHandler
         source = new DualList<char>();
         source.AddEmpty();
 
-        if (content.Length >= 32 && Unsafe.As<byte, uint>(ref MemoryMarshall.GetReference(content)) == 0x04030201U)
+        if (content.Length >= 32 && Unsafe.As<byte, uint>(ref MemoryMarshal.GetReference(content)) == 0x04030201U)
         {
             CryptUtility.Decrypt(content.Slice(4, 28), content.Slice(32));
             content = content.Slice(32);
@@ -63,6 +63,104 @@ public static class UnicodeHandler
                 return;
             }
 
+            lastLine.AddRange(charSpan.Slice(0, linefeedIndex - ((linefeedIndex > 0 && charSpan[linefeedIndex - 1] == '\r') ? 1 : 0)));
+            source.AddEmpty();
+            charSpan = charSpan.Slice(linefeedIndex + 1);
+        } while (!charSpan.IsEmpty);
+    }
+
+    public static async ValueTask<DualList<char>> LoadAsync(SafeFileHandle handle, CancellationToken token)
+    {
+        token.ThrowIfCancellationRequested();
+        var source = new DualList<char>();
+        source.AddEmpty();
+        var length = RandomAccess.GetLength(handle);
+        if (length == 0L) { return source; }
+        const int stride = 1024 * 7 * 8;
+        var buffer = ArrayPool<byte>.Shared.Rent(4 + 28 + stride);
+        byte[]? cryptBuffer = null;
+        try
+        {
+            const int firstReadSize = 4 + 28 + 32 * 7;
+            if (legnth < firstReadSize)
+            {
+                _ = await RandomAccess.ReadAsync(handle, buffer.AsMemory(0, (int)length), 0, token).ConfigureAwait(false);
+                if (length >= 32 && Unsafe.As<byte, uint>(ref buffer) == 0x04030201U)
+                {
+                    CryptUtility.Decrypt(buffer.AsSpan(4, 28), buffer.AsSpan(32, (int)length - 32));
+                    InnerLoad(ref source, buffer.AsSpan(32, (int)length - 32), token);
+                }
+                else
+                {
+                    InnerLoad(ref source, buffer.AsSpan(0, (int)length), token);
+                }
+                return source;
+            }
+
+            _ = await RandomAccess.ReadAsync(handle, buffer.AsMemory(0, firstReadSize), 0, token).ConfigureAwait();
+            if (Unsafe.As<byte, uint>(ref buffer[0]) == 0x04030201)
+            {
+                CryptUtility.Decrypt(buffer.AsSpan(4, 28), buffer.AsSpan(32, firstReadSize - 32));
+                InnerLoad(ref source, buffer.AsSpan(32, firstReadSize - 32), token);
+                cryptBuffer = ArrayPool<byte>.Shared.Rent(56);
+                buffer.AsSpan(4, 28).CopyTo(cryptBuffer);
+                buffer.AsSpan(4, 28).CopyTo(cryptBuffer.AsSpan(28));
+                var loopCount = Math.DivRem(length - firstReadSize, stride, out var remainder);
+                for (long i = 0, offset = firstReadSize; i < loopCount; i++, offset += stride)
+                {
+                    _ = await RandomAccess.ReadAsync(handle, buffer.AsMemory(0, stride), offset, token).ConfigureAwait(false);
+                    CryptUtility.Decrypt(cryptBuffer.AsSpan(0, 56), buffer.AsSpan(0, stride));
+                    InnerLoad(ref source, buffer.AsSpan(0, stride), token);
+                }
+                if (remainder != 0)
+                {
+                    var actual = await RandomAccess.ReadAsync(handle, buffer.AsMemory(0, remainder), length - remainder, token);
+                    CryptUtility.Decrypt(cryptBuffer.AsSpan(0, 56), buffer.AsSpan(0, actual));
+                    InnerLoad(ref source, buffer.AsSpan(0, actual), token);
+                }
+            }
+            else
+            {
+                InnerLoad(ref source, buffer.AsSpan(0, firstReadSize), token);
+                long offset = firstReadSize;
+                do
+                {
+                    var actual = await RandomAccess.ReadAsync(handle, buffer.AsMemory(), offset, token).ConfigureAwait(false);
+                    if (actual == 0)
+                    {
+                        break;
+                    }
+                    actual &= -2;
+                    InnerLoad(ref source, buffer.AsSpan(0, actual), token);
+                    offset += actual;
+                } while (true);
+            }
+            return source;
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+            if (cryptBuffer is not null)
+            {
+                ArrayPool<byte>.Shared.Return(cryptBuffer);
+            }
+        }
+    }
+
+    private static void InnerLoad(ref DualList<char> source, ReadOnlySpan<byte> input, CancellationToken token)
+    {
+        var span = MemoryMarshal.Cast<byte, char>(input);
+        do
+        {
+            token.ThrowIfCancellationRequested();
+            var linefeedIndex = charSpan.IndexOf('\n');
+            ref var lastLine = ref source.Last;
+            if (linefeedIndex < 0)
+            {
+                lastLine.AddRange(charSpan);
+                return;
+            }
+        
             lastLine.AddRange(charSpan.Slice(0, linefeedIndex - ((linefeedIndex > 0 && charSpan[linefeedIndex - 1] == '\r') ? 1 : 0)));
             source.AddEmpty();
             charSpan = charSpan.Slice(linefeedIndex + 1);
